d36f287f9bb6507d806b434a428f2aada3ec9f40
soramati <48977898+soramati@users.noreply.github.com>
2024-11-17 21:40:54 +0900

    バリデーションエラー表示の実装

--- src/app/Http/Requests/GoalRequest.php
+++ src/app/Http/Requests/GoalRequest.php
@@ -11,20 +11,20 @@ class GoalRequest extends FormRequest
     public function rules(): array
     {
         return [
-            // 'goals_name' => 'required|string|max:20',
-            // 'goals_is_deadline'=>  'boolean',
-            // 'goals_deadline' => 'date',
-            // 'goals_reward', 'string|max:200',
-            // 'goals_conditions' => 'string|max:200',
-            // 'goals_repeat' => 'boolean',
-            // 'goals_point' => 'integer|between:0,100',
-            // 'goals_is_achieved' => 'boolean',
-            // 'goals_percent' => 'integer|between:0,100',
-            // 'goals_is_set'  => 'boolean',
-            // 'users_id' => 'integer|min:1',
-            // 'goals_user_id' => 'integer|min:1',
-            
-            //
+            // 下記のバリデーションにひっかかると、エラーメッセージが返るはずなのに、何も返ってこない
+            'goal.goals_name' => 'required|string|max:20',
+            'goal.goals_is_deadline'=>  'boolean',
+            'goal.goals_deadline' => 'date',
+            'goal.goals_reward', 'string|max:200',
+            'goal.goals_conditions' => 'string|max:200',
+            'goal.goals_repeat' => 'boolean',
+            'goal.goals_point' => 'integer|between:0,100',
+            'goal.goals_is_achieved' => 'boolean',
+            'goal.goals_percent' => 'integer|between:0,100',
+            'goal.goals_is_set'  => 'boolean',
+            'goal.users_id' => 'integer|min:1',
+            'goal.goals_user_id' => 'integer|min:1',
+                       
         ];
     }
 }
